d4ddea9645a701e5630f1cb6aa1f52b483dbe507
Florian Scherf <mail@florianscherf.de>
2025-04-30 22:20:13 +0200

    frontend: eslint: disable unused-vars errors for function declarations
    Previously, eslint would complain when a function specified an argument that
    it then wouldn't use, which becomes an issue when you need to implement a
    specific interface or need to be backwards compatible.
    
    Signed-off-by: Florian Scherf <mail@florianscherf.de>

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -7,5 +7,6 @@ import { defineConfig } from "eslint/config";
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
+  { rules: {"@typescript-eslint/no-unused-vars": [ "error", { "args": "none" } ] } },
   tseslint.configs.recommended,
 ]);
